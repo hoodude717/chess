561cc9275244ef18496d949d93148396d5d033cb
hoodude717 <156257453+hoodude717@users.noreply.github.com>
2025-10-01 20:05:56 -0600

    Implemented Checkmate

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,8 +1,6 @@
 package chess;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -15,11 +13,23 @@ public class ChessGame {
     private TeamColor teamTurn = TeamColor.WHITE;
     private ChessBoard board = new ChessBoard();
     private Collection<ChessMove> allMoves = new ArrayList<>(); //Used to keep track of all the moves in the game
-    private ChessPosition whiteKingPos = new ChessPosition(1, 5);
-    private ChessPosition blackKingPos = new ChessPosition(8, 5);
+    protected ChessPosition whiteKingPos = new ChessPosition(1, 5);
+    protected ChessPosition blackKingPos = new ChessPosition(8, 5);
 
     public ChessGame() {
         board.resetBoard();
+    }
+    public ChessGame(ChessBoard newBoard) {
+        board = newBoard;
+    }
+
+    // Copy constructor
+    public ChessGame(ChessGame original) {
+        this.board = new ChessBoard(original.board);
+        this.blackKingPos = original.blackKingPos;
+        this.whiteKingPos = original.whiteKingPos;
+        this.teamTurn = original.teamTurn;
+        this.allMoves = original.allMoves;
     }
 
     /**
@@ -56,10 +66,47 @@ public class ChessGame {
     public Collection<ChessMove> validMoves(ChessPosition startPosition) {
         ChessPiece piece = board.getPiece(startPosition);
         Collection<ChessMove> moves = new ArrayList<>();
-        if (piece != null) {
-            moves = piece.pieceMoves(board, startPosition);
+        if (piece == null) return List.of();
+        TeamColor pieceColor = piece.getTeamColor();
+        moves = piece.pieceMoves(board, startPosition);
+        Collection<ChessMove> movesCopy =  new ArrayList<>();
+        for (ChessMove move : moves) {
+            if (checkValidMove(move, pieceColor)) {
+                movesCopy.add(move);
+            }
         }
-        return moves;
+        return movesCopy;
+    }
+
+
+    /**
+     * Gets a valid moves for a piece at the given location
+     *
+     * @param move the move that needs to be checked.
+     * @return whether the move would put the king in check or not
+     */
+    private boolean checkValidMove(ChessMove move, TeamColor teamColor) {
+        ChessGame tempGame = new ChessGame(this);
+        //Move the piece in the tempGame to check if it would cause check
+        try { tempGame.movePiece(move); }
+        catch (InvalidMoveException i) { return false;}
+        return !tempGame.isInCheck(teamColor);
+    }
+
+    //Move the piece without the checks
+    protected void movePiece(ChessMove move) throws InvalidMoveException {
+        ChessPosition start = move.getStartPosition();
+        ChessPiece piece = board.getPiece(start);
+        if (piece == null) throw new InvalidMoveException("No Piece at start Position");
+        if(move.getPromotionPiece() != null) {//Upgrade if promotion piece
+            board.addPiece(move.getEndPosition(), new ChessPiece(piece.getTeamColor(), move.getPromotionPiece()));
+        } else {
+            if (piece.getPieceType() == ChessPiece.PieceType.KING) updateKingPosition(move.getEndPosition());
+            board.addPiece(move.getEndPosition(), piece);
+        }
+        board.addPiece(start, null); //Clear old space
+        if (teamTurn == TeamColor.WHITE) { teamTurn = TeamColor.BLACK; } //Switch turns
+        else {teamTurn = TeamColor.WHITE; }
     }
 
     /**
@@ -71,19 +118,14 @@ public class ChessGame {
     public void makeMove(ChessMove move) throws InvalidMoveException {
         ChessPosition start = move.getStartPosition();
         ChessPiece piece = board.getPiece(start);
+        if (piece == null) throw new InvalidMoveException("No Piece at start space");
         Collection<ChessMove> possibleMoves = validMoves(start);
-        if (possibleMoves.isEmpty()) {throw new InvalidMoveException("No Possible Moves");}
+        //Check if the move is for a piece with the correct color
         if (teamTurn != piece.getTeamColor()) { throw new InvalidMoveException("Wrong Color's Turn"); }
+        if (possibleMoves.isEmpty()) {throw new InvalidMoveException("No Possible Moves");}
         if (possibleMoves.contains(move)) {
-            if(move.getPromotionPiece() != null) {//Upgrade if promotion piece
-                board.addPiece(move.getEndPosition(), new ChessPiece(piece.getTeamColor(), move.getPromotionPiece()));
-            } else {
-                if (piece.getPieceType() == ChessPiece.PieceType.KING) updateKingPosition(move.getEndPosition());
-                board.addPiece(move.getEndPosition(), piece);
-            }
-            board.addPiece(start, null); //Clear old space
-            if (teamTurn == TeamColor.WHITE) { teamTurn = TeamColor.BLACK; } //Switch turns
-            else {teamTurn = TeamColor.WHITE; }
+            try{movePiece(move);}
+            catch (InvalidMoveException e) {throw new InvalidMoveException("No Piece at Start");}
             allMoves.add(move);
         } else {
             throw new InvalidMoveException("Move would put king in danger");
@@ -106,14 +148,14 @@ public class ChessGame {
      */
     public boolean isInCheck(TeamColor teamColor) {
         ChessPosition kingPosition;
-        if (teamColor == TeamColor.WHITE) kingPosition = whiteKingPos; //TODO: Implement getKing functions inside Game
+        if (teamColor == TeamColor.WHITE) kingPosition = whiteKingPos;
         else kingPosition = blackKingPos;
 
         for (int i=1; i<=8; i++ ) { // Check row by row
             for (int j=1; j<=8; j++) { // Check columns
                 ChessPosition space = new ChessPosition(i,j);
                 if (board.isSpaceEmpty(space)) {
-                    continue;
+                     continue;
                 }
 
                 ChessPiece piece = board.getPiece(space);
@@ -137,7 +179,21 @@ public class ChessGame {
      * @return True if the specified team is in checkmate
      */
     public boolean isInCheckmate(TeamColor teamColor) {
-        return false;
+        boolean inCheckmate = false;
+        if (getTeamTurn() != teamColor) return false; //Can only be in checkmate on your turn
+        if (!isInCheck(teamColor)) return false; // Can only be in checkmate if check first.
+        //Can only be in checkmate if no valid moves for all pieces on the teamColor team
+        for (int i=1; i<=8; i++ ) { // Check row by row
+            for (int j=1; j<=8; j++) { // Check columns
+                ChessPiece piece =  board.getPiece(new ChessPosition(i, j));
+                if (board.isSpaceEmpty(new ChessPosition(i,j))) continue; //Check empty space
+                if (piece.getTeamColor() != teamColor) continue; //Check if opposite team
+
+                if (!validMoves(new ChessPosition(i, j)).isEmpty()) return false;
+
+            }
+        }
+        return true;
     }
 
     /**
@@ -171,7 +227,6 @@ public class ChessGame {
                     } else {
                         blackKingPos = curPos;
                     }
-                    break;
                 }
             }
         }
