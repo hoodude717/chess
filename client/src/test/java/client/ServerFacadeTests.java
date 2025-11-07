9443dbe8e6775dc3fcb97fc44e11522c5d796b19
hoodude717 <156257453+hoodude717@users.noreply.github.com>
2025-11-06 20:22:50 -0700

    Beginning to create the ServerFacade and the register test for it

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -1,18 +1,26 @@
 package client;
 
+import exceptions.ResponseException;
 import org.junit.jupiter.api.*;
 import server.Server;
+import client.ServerFacade;
+import servicerequests.RegisterRequest;
+import serviceresults.RegisterResult;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
+    static ServerFacade serverFacade;
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        var serverUrl = "http://localhost:" + port;
+
+        serverFacade = new ServerFacade(serverUrl);
     }
 
     @AfterAll
@@ -20,6 +28,18 @@ public class ServerFacadeTests {
         server.stop();
     }
 
+
+    @Test
+    public void registerGood() {
+        RegisterRequest request = new RegisterRequest("bradford", "bradford", "bradford");
+        Assertions.assertDoesNotThrow(()->serverFacade.register(request));
+    }
+
+    @Test
+    public void registerBad() {
+        RegisterRequest request = new RegisterRequest("bradford", "Bradford", "bradford");
+        Assertions.assertThrows(ResponseException.class, ()->serverFacade.register(request));
+    }
 
     @Test
     public void sampleTest() {
